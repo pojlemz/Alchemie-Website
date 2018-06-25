8f4d5d82060a984dd74bd374ffcb04765c6a053f
dblockunity <33234738+dblockunity@users.noreply.github.com>
2018-06-25 18:08:09 -0400

    Test written for Dillon Gage history viewing.

--- test/script-dillon-gage-get-history.js
+++ test/script-dillon-gage-get-history.js
@@ -3,10 +3,11 @@ require('dotenv').config({path: '../.env'});
 const request = require('request');
 const host = process.env.DILLON_GAGE_API_ENDPOINT;
 const publicToken = process.env.DILLON_GAGE_PRIVATE_TOKEN;
+const numFizTradeNumber = "FIZ00063480";
 
 // 100G
 // var url = host + '/FizServices/GetPrices/'+publicToken+'/1GP';
-var url = host + '/FizServices/GetPrices/'+publicToken+'/1KILOG';
+var url = host + '/FizServices/GetTrade/'+publicToken+'/'+numFizTradeNumber;
 request({
     uri: url,
     method: ""
