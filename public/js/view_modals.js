5626cf2f60bac79a6d8e8bdc6c74c7b7dc6c3135
dblockunity <33234738+dblockunity@users.noreply.github.com>
2018-07-09 17:18:11 -0400

    Little animation added to UI

--- public/js/view_modals.js
+++ public/js/view_modals.js
@@ -8,6 +8,7 @@ ViewModals.prototype.initialize = function(){
 
 ViewModals.prototype.showModal = function(modalName){
     $("#fn-modal-overlay").show();
+    $(".fn-modal-user-message-box").empty();
     var modal = $("."+modalName);
     modal.show();
 }
@@ -15,11 +16,14 @@ ViewModals.prototype.showModal = function(modalName){
 // g_App.getViewModals().showErrorMessageInModal("fn-2fa-not-attemptable", "This is an error message");
 ViewModals.prototype.showErrorMessageInModal = function(modalName, errorMessage){
     $("#fn-modal-overlay").show();
-    var modal = $("."+modalName + ' .fn-modal-user-message-box');
-    modal.empty();
+    var messageBox = $("."+modalName + ' .fn-modal-user-message-box');
+    messageBox.empty();
     var html = '<div class="css-modal-user-message fn-modal-user-message">';
     html += errorMessage;
     html += '</div>';
-    modal.append(html);
+    messageBox.append(html);
+    setTimeout(function(){
+        messageBox.children().slideUp();
+    }, 5000)
     // TODO: Set timeout to make error message disappear in 5 seconds.
 }
