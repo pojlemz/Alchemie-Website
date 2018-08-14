fd2104c1c4e9ad518fa1c89db0963a71dd56487d
dblockunity <33234738+dblockunity@users.noreply.github.com>
2018-08-14 15:31:57 -0400

    Comments added

--- routes/forgotten-password.js
+++ routes/forgotten-password.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('../server/passport');
+var express = require('express'); // Express app
+var router = express.Router(); // Express router
+var passport = require('../server/passport'); // Set variable to the node module passport
 
-const parseForm = require('../server/parse-form');
+const parseForm = require('../server/parse-form'); // Used to scan requests for valid CSRF tokens
 
 // Login
 // router.post('/login', function(req, res) {
 //     passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true})
 //     //res.redirect('/');
 // });
-router.post('/forgotten-password',parseForm,function(req, res) {
-    res.redirect('/forgotten-password');
+router.post('/forgotten-password',parseForm,function(req, res) { // Route called when user makes a post request to get the page where they can report a forgotten password
+    res.redirect('/forgotten-password'); // Redirect user to 'get' route
 });
 
-router.get('/forgotten-password', function(req, res){
-    res.render('forgotten-password', {});
+router.get('/forgotten-password', function(req, res){ // Route called when user makes a get request to get the page where they can report a forgotten password.
+    res.render('forgotten-password', {}); // Render page where user can enter forgotten password
 });
 
 module.exports = router;
