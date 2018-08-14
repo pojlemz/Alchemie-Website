575c341c71686f90c570a22cfc5e1b70f1e05da9
dblockunity <33234738+dblockunity@users.noreply.github.com>
2018-08-14 14:57:26 -0400

    Comments added

--- routes/complete-registration.js
+++ routes/complete-registration.js
@@ -1,40 +1,40 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); // Express app
+var router = express.Router(); // Express router
 
-var User = require('../models/user');
-var PendingUser = require('../models/pending-user');
+var User = require('../models/user'); // User postgres table
+var PendingUser = require('../models/pending-user'); // Pending user postgres table
 
-router.get('/complete-registration', function(req, res){
-    var randomHash = req.query.key;
-    console.log('Success entering complete-registration route');
-    PendingUser.getUserByHashKey(randomHash, function(err, data){
-        if (err){
-            req.flash('error_msg', 'Error while reading user key.');
-            res.redirect('/login');
-        } else {
-            if (data === null) {
-                req.flash('error_msg', 'This key does not correspond to any pending user.');
-                res.redirect('/login');
-            } else {
-                console.log('Success finding a key.');
-                PendingUser.deleteUserByHashKey(randomHash, function (err) {
-                    if (err) {
-                        req.flash('error_msg', 'Error while removing user pending registration.');
-                        res.redirect('/login');
-                    } else {
+router.get('/complete-registration', function(req, res){ // complete registration route - runs when a user clicks on a link sent to their email
+    var randomHash = req.query.key; // Sets variable to the code associated with the registration created earlier
+    console.log('Success entering complete-registration route'); // Log message to enable easier debugging
+    PendingUser.getUserByHashKey(randomHash, function(err, data){ // Get user corresponding to random hash in url
+        if (err){ // If the postgres request gives us some kind of error
+            req.flash('error_msg', 'Error while reading user key.'); // Set up flash message to show an error to the user.
+            res.redirect('/login'); // Redirect to the login route
+        } else { // If there is no error
+            if (data === null) { // If the hash does not correspond to any user
+                req.flash('error_msg', 'This key does not correspond to any pending user.'); // Show the user a message saying the hash doesn't correspond to any user
+                res.redirect('/login'); // Redirect to the login route
+            } else { // If the hash does correspond to a user
+                console.log('Success finding a key.'); // Display a message proclaiming success that a key has been found
+                PendingUser.deleteUserByHashKey(randomHash, function (err) { // Remove the corresponding user from the pending user table
+                    if (err) { // If there is an error in this Postgres request
+                        req.flash('error_msg', 'Error while removing user pending registration.'); // Tell the user that there was some kind of error when removing the Postgres user
+                        res.redirect('/login'); // Redirect to the login route
+                    } else { // If there is no error
                         // Add user to users table
                         // @TODO handle error here.
-                        var newUser = {
-                            name: data.name,
-                            email: data.email,
-                            password: data.password
+                        var newUser = { // Create an object called new user
+                            name: data.name, // Set name to the pending user's name
+                            email: data.email, // Set email to the pending user's email
+                            password: data.password // Set password to the pending user's password
                         };
-                        User.createUser(newUser, function (err, user) {
-                            if (err) throw err;
-                            console.log(user);
+                        User.createUser(newUser, function (err, user) { // Using the Postgres model User, create a new user with the newUser object
+                            if (err) throw err; // If there is an error then throw an exception
+                            console.log(user); // If there is no error then print the user object
                         });
-                        req.flash('success_msg', 'You are registered and can now login.');
-                        res.redirect('/login');
+                        req.flash('success_msg', 'You are registered and can now login.'); // Display a message to the user saying that they can now log in
+                        res.redirect('/login'); // Redirect user to the login page
                     }
                 });
             }
--- routes/forgotten-password-submitted.js
+++ routes/forgotten-password-submitted.js
@@ -1,38 +1,39 @@
-var express = require('express');
-var router = express.Router();
-var host = require('../server/host');
-var request = require('request');
+var express = require('express'); // Express app
+var router = express.Router(); // Express router
+var host = require('../server/host'); // Set variable to the web address of the host (server) - used for making calls from the front end
+var request = require('request'); // Set variable to the node module used for making requests
 
-var generateRandomHash = require('../server/generate-random-hash');
-var messageSender = require('../server/emailer/message-sender');
+var generateRandomHash = require('../server/generate-random-hash'); // Set variable to a function that generates random hashes
+var messageSender = require('../server/emailer/message-sender'); // Set variable to a function responsible for sending emails using Google's API
 // var secretKey = require('../keys/// var secretKey = require('../keys/recaptcha');recaptcha');
-var secretKey = process.env.GOOGLE_RECAPTCHA;
-var ForgottenPasswordLinks = require('../models/forgotten-password-link');
-var User = require('../models/user');
+var secretKey = process.env.GOOGLE_RECAPTCHA; // Set secret key variable to key used for Google's captcha
+var ForgottenPasswordLinks = require('../models/forgotten-password-link'); // Set variable to match the forgottenpasswordlink table in postgres
+var User = require('../models/user'); // Set variable to work with postgres 'User' table
 
-const parseForm = require('../server/parse-form');
+const parseForm = require('../server/parse-form'); // Function used for ensuring the CSRF token provided is valid
 
-router.post('/forgotten-password-submitted',parseForm,function(req, res) {
-    req.checkBody('email', 'Email required').notEmpty();
-    var errors = req.validationErrors();
-    console.log(errors);
+router.post('/forgotten-password-submitted',parseForm,function(req, res) { // Route called when the user reports that they have forgotten their password
+    req.checkBody('email', 'Email required').notEmpty(); // Ensures that the email body parameter is provided in the POST request
+    var errors = req.validationErrors(); // Set variable to be all the validation errors that could come with the request
+    console.log(errors); // Print the errors - was used for debugging at some point
 
     // @TODO: Anti spam functionality. Only allow 3 emails to be sent every 15 minutes.
-    if(errors){
-        res.render('login',{
-            errors:errors
+    if(errors){ // If there are validation errors
+        res.render('login',{ // Show the login page also reporting the validation errors that were found
+            errors:errors // Include the errors as a parameter
         });
-    } else {
+    } else { // If there are no validation errors
+        // If the request body contains the captcha response
         if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-            return res.json({"responseCode" : 1,"responseDesc" : "Please select the captcha"});
+            return res.json({"responseCode" : 1,"responseDesc" : "Please select the captcha"}); // Send back a response telling the user to select the captcha
         }
         // req.connection.remoteAddress will provide IP address of connected user.
         var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
         // Hitting GET request to the URL, Google will respond with success or error scenario.
-        request(verificationUrl, function(error, response, body) {
-            body = JSON.parse(body);
+        request(verificationUrl, function(error, response, body) { // Send a request to the verification url
+            body = JSON.parse(body); // Set body to a version of body parsed with JSON
             // Success will be true or false depending upon captcha validation.
-            if(body.success !== undefined && !body.success) {
+            if(body.success !== undefined && !body.success) { // If body has a success parameter
                 // This code block will run if the user doesn't fill in the captcha.
                 return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification."}); // Uncomment this line to see what the old code did.
             }
@@ -40,33 +41,33 @@ router.post('/forgotten-password-submitted',parseForm,function(req, res) {
             // res.json({"responseCode" : 0,"responseDesc" : "Success"}); // Uncomment this line to see what the old code did.
 
             // We now check that the entered email actually exists.
-            var email = req.body.email;
-            User.getUserByEmail(email, function (req2, res2){
-                if (res2 === null){
+            var email = req.body.email; // Set variable to email parameter of original client request
+            User.getUserByEmail(email, function (req2, res2){ // Get postgres user information using user email
+                if (res2 === null){ // If the response is null
                     // req.flash('error_msg', 'This user is not registered');
-                    res.render('user-does-not-exist');
+                    res.render('user-does-not-exist'); // Render the page letting the user know that the user doesn't exist (for easy debugging)
                     // return res.json({"responseCode" : 1,"responseDesc" : "This user is not registered."}); // Uncomment this line to see what the old code did.
-                } else {
+                } else { // If the response is not null
                     // Now we send an email with a random link and we store the random link in a database.
-                    administerPasswordLink(req, res)
+                    administerPasswordLink(req, res) // Call a function that sends a link to the user's email instructing them to reset their password.
                 }
             });
         });
-
     }
 });
 
-function administerPasswordLink(req, res){
-    var email = req.body.email;
-    var currentTime = new Date().getTime();
-    var expiryTime = currentTime + 1000 * 60 * 30;
-    var randomHash = generateRandomHash();
-    var url = host + '/reset-your-password?key=' + randomHash;
-    var messageSubject = 'Resetting your password with Block Unity';
+function administerPasswordLink(req, res){ // Function called that sends link to the client's email address with instructions to reset their password
+    var email = req.body.email; // Set email variable to be email entered in form on webpage
+    var currentTime = new Date().getTime(); // Set current time variable to equal the milliseconds since unix epoch for the current time
+    var expiryTime = currentTime + 1000 * 60 * 30; // Set the expiry time to equal the current time plus 30 minutes
+    var randomHash = generateRandomHash(); // Set variable to equal a random hash that has been generated by a function
+    var url = host + '/reset-your-password?key=' + randomHash; // Set url for resetting password to go to the reset-your-password route at host with the random hash generated
+    var messageSubject = 'Resetting your password with Block Unity'; // Set the message subject for the email message.
+    // Set the message body for the email in the next line which includes the 'url' variable
     var messageBody = "Greetings from the Block Unity team!\n\n We received a request to reset the password corresponding to this email address.\n\n If this was you then please click on the link found at " + url + " within thirty minutes to reset your password. \n\n If you didn't make this request then feel free to ignore this email. \n\n" +"Best Regards,"+"\n\n" +"The Block Unity Team";
-    ForgottenPasswordLinks.createPasswordResetLink({email: email, passwordLink: url, expiryMillisecondsSinceUnixEpoch: expiryTime}, function(err2, res2) {
-        messageSender(email, messageSubject, messageBody, function (err, content) {
-            res.render('forgotten-password-submitted');
+    ForgottenPasswordLinks.createPasswordResetLink({email: email, passwordLink: url, expiryMillisecondsSinceUnixEpoch: expiryTime}, function(err2, res2) { // Create a passowrd reset link in the Postgres table corresponding to the random hash.
+        messageSender(email, messageSubject, messageBody, function (err, content) { // Use the message sender function to send an email to the client's email with provided message subject and message body.
+            res.render('forgotten-password-submitted'); // Render the page that informs the user their forgotten password has been submitted
         });
     });
 }
